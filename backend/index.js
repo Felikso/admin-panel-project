230536de37e8a1cb5493fe1f4529b76030246fd0
Ola <xfeliksox@wp.pl>
2024-11-17 09:21:32 +0100

    cros error

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 	}
 	res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, POST');
 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-	//res.header('Access-Control-Allow-Headers', '*');
+	res.header('Access-Control-Allow-Headers', '*');
 	res.header('Access-Control-Allow-Credentials', true);
 	return next();
   });
