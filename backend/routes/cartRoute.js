7be59c9f777e16976ccd983f1b699746cc534cbf
Ola <xfeliksox@wp.pl>
2024-11-17 12:26:06 +0100

    cors edited

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import { addToCart, removeFromCart, getCart } from '../controllers/cartController.js'
-import { authMiddleware } from '../middleware/auth.js'
+import { authMiddleware, authOrderMiddleware } from '../middleware/auth.js'
 import { verifyToken } from '../middleware/verifyToken.js'
 
 const cartRoute = express.Router();
 
-cartRoute.post('/add',authMiddleware,addToCart)
-cartRoute.post('/remove',authMiddleware,removeFromCart)
-cartRoute.post('/get',authMiddleware,getCart)
+cartRoute.post('/add',authOrderMiddleware,addToCart)
+cartRoute.post('/remove',authOrderMiddleware,removeFromCart)
+cartRoute.post('/get',authOrderMiddleware,getCart)
 
 export default cartRoute;
